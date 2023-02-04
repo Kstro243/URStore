033525de6c143ca50f64f7b40ae843eaea2b43d1
Juan Castro <jcastro2433@gmail.com>
2023-02-03 20:30:56 -0500

    Realicé el header y las shopping cards, quiero cambiar el color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingCardsComponent } from './components/shopping-cards/shopping-cards.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCardsComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
