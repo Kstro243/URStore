51deafcbac7b65f8f1ba867819a59171e73d41c1
Juan Castro <jcastro2433@gmail.com>
2023-02-03 20:32:00 -0500

    Se me olvidó agregar los archivos nuevosxd

---
+++ src/app/components/shopping-cards/shopping-cards.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/product.module';
+
+@Component({
+  selector: 'app-shopping-cards',
+  templateUrl: './shopping-cards.component.html',
+  styleUrls: ['./shopping-cards.component.scss']
+})
+export class ShoppingCardsComponent {
+  products: Producto[] = [
+    {
+      name: 'Cosa para vender',
+      price: 123,
+      image: './assets/descarga.jpg',
+      category: 'all',
+    }, {
+      name: 'Cosa para vender',
+      price: 123,
+      image: './assets/descarga.jpg',
+      category: 'all',
+    }, {
+      name: 'Cosa para vender',
+      price: 123,
+      image: './assets/descarga.jpg',
+      category: 'all',
+    }, {
+      name: 'Cosa para vender',
+      price: 123,
+      image: './assets/descarga.jpg',
+      category: 'all',
+    }, {
+      name: 'Cosa para vender',
+      price: 123,
+      image: './assets/descarga.jpg',
+      category: 'all',
+    }, {
+      name: 'Cosa para vender',
+      price: 123,
+      image: './assets/descarga.jpg',
+      category: 'all',
+    }, {
+      name: 'Cosa para vender',
+      price: 123,
+      image: './assets/descarga.jpg',
+      category: 'all',
+    },
+  ]
+}
